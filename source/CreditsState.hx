cb4bfa867120ec3597e067d1a9ce9e50cbe45343
gralegra <100141683+gralegra@users.noreply.github.com>
2022-03-18 14:13:36 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -65,12 +65,6 @@ class CreditsState extends MusicBeatState
 			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",					'https://twitter.com/PhantomArcade3K',	'0xFFFFBB1B'],
 			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",					'https://twitter.com/evilsk8r',			'0xFF53E52C'],
 			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",					'https://twitter.com/kawaisprite',		'0xFF6475F3'
-			['Team Plotsh'],
-			['Plotsh',		    '',		    'Pessoa Legal',	 'https://youtube.com/c/Plotsh',	'0xFFC30085'],
-			['Moxie',		'',		'Idiota Total',					'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
-			['Caio',			'',		'O Artista',				'',		'0xFFC30085'],
-			['Enzo',			'',		'O Dono Do Serve',				'https://discord.gg/436c6Emptn',		'0xFFC30085'],
-			['Pedro',			'',		'Amigo Muito Legal',				'',		'0xFFC30085'],
 			[''],
 		];
 		
